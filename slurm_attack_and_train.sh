3c84a476944d7caf1180d0eb86c410f5ef5f14d0
Jan Zahálka <jan@zahalka.net>
2023-11-04 15:06:57 +0100

    Reformulated the SLURM script to run on the general GPU partition (DGXs seem to be down today).

--- slurm_attack_and_train.sh
+++ slurm_attack_and_train.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -l
 #SBATCH --job-name=trwrck
 #SBATCH --partition gpu
-#SBATCH --gres=gpu:Volta100:1
+#SBATCH --exclude=node12
 #SBATCH --mem=16GB
 #SBATCH -o ./slurm_outputs/trwrck-%A_%a.out
 
 source ${SLURM_SUBMIT_DIR}/env/bin/activate
 python attack_and_train.py
+
+#(SBATCH --gres=gpu:Volta100:1)
